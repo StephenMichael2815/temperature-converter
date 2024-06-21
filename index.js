7ce0f1369e96f1202c7ed08b227d49bea90ab4fe
Stephen Michael <smichael2815@Stephens-MacBook-Pro.local>
2024-06-21 13:01:11 -0600

    updated js file

--- index.js
+++ index.js
@@ -5,7 +5,11 @@
  * @param {string} fahren temperature in degrees F
  * @returns {number} the number of degrees C
  */
-function convertToCelsius(fahren) {}
+function convertToCelsius(fahren) {
+  /**converting the value from F to C subtract 32 from the F value, then multiply it by 5/9 */
+  const celsius = ((fahren - 32) * 5) / 9;
+  return celsius.toFixed(2);
+}
 
 /**
  * Takes both numbers (F, C) and display a message with
@@ -22,14 +26,30 @@ function convertToCelsius(fahren) {}
  * @param {number} fahren
  * @param {number} celc
  */
-function createMessage(fahren, celc) {}
+function createMessage(fahren, celc) {
+  let sentiment = "";
+  if (fahren < 32) {
+    sentiment = "very cold";
+  } else if (fahren < 64) {
+    sentiment = "cold";
+  } else if (fahren < 86) {
+    sentiment = "warm";
+  } else if (fahren < 100) {
+    sentiment = "hot";
+  } else {
+    sentiment = "very hot";
+  }
+  return `The temperature in F is ${fahren} and in C is ${celc}. It feels  ${sentiment}.`;
+}
 
 /**
  * Takes a number and returns a random integer from 0 to the limit
- * @param {number} limit
+ * @pa  ram {number} limit
  * @returns {number} a number between 0 and the int passed in
  */
-function rand(limit) {}
+function rand(limit) {
+  return Math.floor(Math.random() * limit);
+}
 
 // -------------------- DO NOT CHANGE THE CODE BELOW ---------------------- //
 
